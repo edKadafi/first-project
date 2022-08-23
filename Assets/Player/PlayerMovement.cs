61473f07071ea04ff2b75879713e1fd0d01eb85e
edKadafi <frpambou@Yahoo.com>
2022-08-23 22:32:56 +0300

    Jump only from ground + Fall damage

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -12,13 +12,16 @@ namespace Proiect.Player
         private Transform PlayerTransform;
         [SerializeField]
         private Transform CameraOrient;
-        [SerializeField, Range(0, 100)] private float movementSpeed;
+        [SerializeField, Range(0, 1000)] private float movementSpeed;
         // [SerializeField] private CharacterController _characterController;
-        private float turnSmoothTime = 0.1f;
+        private float turnSmoothTime = 0.05f;
         private float turnSmoothVelocity;
         private Rigidbody body;
         private Vector3 velocity;
         private KeyCode jumpKey = KeyCode.Space;
+        private int moveState = 0;
+        private bool CanJump = false;
+        private float fallVelocity = 0f;
 
         private void Awake()
         {
@@ -34,12 +37,10 @@ namespace Proiect.Player
         
         void Update()
         {
-            body.velocity += new Vector3(0f, -9.81f*3, 0f)*Time.deltaTime;
             if (Input.GetKeyDown(jumpKey))
             {
                 JumpPlayer();
             }
-
         }
 
         void MovePlayer()
@@ -59,20 +60,71 @@ namespace Proiect.Player
                 this.transform.rotation = Quaternion.Euler(0f, angle, 0f);
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
                 velocity = movementSpeed * moveDir.normalized;
+                body.AddForce(velocity, ForceMode.Acceleration);
                 // _characterController.Move(Time.deltaTime * movementSpeed * moveDir.normalized);
-                body.velocity += velocity*Time.deltaTime;
+                // body.velocity += velocity*Time.deltaTime;
             }
             
         }
 
         private void FixedUpdate()
         {
+            
+            //Moving the player on every fixed update
             MovePlayer();
+            body.velocity += new Vector3(0f, -9.81f*3, 0f)*Time.deltaTime;
+            RegisterFallVelocity();
         }
 
+        //Handling player jumping
         void JumpPlayer()
         {
-            body.velocity += movementSpeed*(Vector3.up/2);
+            if (CanJump == true)
+            {
+                // body.velocity += movementSpeed*(Vector3.up/2);
+                body.AddForce(movementSpeed*(Vector3.up.normalized/2), ForceMode.Impulse);
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            CheckFallVelocity();
+            fallVelocity = 0f;
+        }
+
+        private void OnCollisionStay(Collision collisionInfo)
+        {
+            //Making sure the player can always jump while on the ground
+            if (collisionInfo.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                CanJump = true;
+            }
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            //Player cannot jump midair
+            if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                CanJump = false;
+            }
+        }
+
+        private void CheckFallVelocity()
+        {
+            Debug.Log(fallVelocity);
+            if (fallVelocity <= -27f)
+            {
+                PlayerManager.DamagePlayer(15);
+            }
+        }
+
+        private void RegisterFallVelocity()
+        {
+            if (CanJump == false)
+            {
+                fallVelocity = body.velocity.y;
+            }
         }
     }
 }
